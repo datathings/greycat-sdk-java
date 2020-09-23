d5da04fdae1d004fc5db340b19d5ada2835aca59
François FOUQUET <francois.fouquet@datathings.com>
2020-09-23 19:35:09 +0200

    major api update for temporal management

--- native/jreference.c
+++ native/jreference.c
@@ -3,11 +3,11 @@
 
 #include <jni.h>
 
-#include <greycat/memory/gref.h>
+#include <greycat/runtime/ref.h>
 #include <greycat/sbi/type.h>
 
 JNIEXPORT jstring JNICALL Java_io_greycat_impl_ReferenceImpl_nToString(JNIEnv *env, jclass class, jlong value) {
-    gref_t ref;
+    gc_rt_ref_t ref;
     ref.raw = value;
     uint32_t len = (uint32_t) snprintf(NULL, 0, "%" PRIu64 ".%" PRIu16, (u64_t) ref.block_id, (u16_t) ref.offset);
     char buffer[len + 1];
