9a183825bacdfe171b7f4cf17da432973c0759d9
François FOUQUET <francois.fouquet@datathings.com>
2020-09-24 10:39:18 +0200

    major cleanup of RT library

--- native/jtensor.c
+++ native/jtensor.c
@@ -2,33 +2,33 @@
 
 #include <greycat/log.h>
 #include <greycat/rt/gtype.h>
-#include <greycat/rt/struct/gtensor.h>
+#include <greycat/rt/tensor.h>
 
 extern jobject jtype__g2j(JNIEnv *env, ggraph_t *graph, gc_rt_slot_t slot, gptype_t slot_type);
 
 JNIEXPORT jint JNICALL Java_io_greycat_impl_TensorImpl_nSize(JNIEnv *env, jclass class, jlong ptr) {
-  return ((gtensor_t *) (intptr_t) ptr)->descriptor.size;
+  return ((gc_rt_tensor_t *) (intptr_t) ptr)->descriptor.size;
 }
 
 JNIEXPORT void JNICALL
 Java_io_greycat_impl_TensorImpl_nRawInitDouble(JNIEnv *env, jclass class, jlong ptr, jint size) {
-  gtensor_t *this = (gtensor_t *) (intptr_t) ptr;
-  gtensor__init(this, gc_sbi_slot_type_f64, (int64_t) size, true, NULL);
+    gc_rt_tensor_t *this = (gc_rt_tensor_t *) (intptr_t) ptr;
+    gc_rt_tensor__init(this, gc_sbi_slot_type_f64, (int64_t) size, true, NULL);
   this->descriptor.nb_dim = 1;
   this->descriptor.dim[0] = size;
 }
 
 JNIEXPORT void JNICALL
 Java_io_greycat_impl_TensorImpl_nRawInitFloat(JNIEnv *env, jclass class, jlong ptr, jint size) {
-  gtensor_t *this = (gtensor_t *) (intptr_t) ptr;
-  gtensor__init(this, gc_sbi_slot_type_f32, (int64_t) size, true, NULL);
+    gc_rt_tensor_t *this = (gc_rt_tensor_t *) (intptr_t) ptr;
+    gc_rt_tensor__init(this, gc_sbi_slot_type_f32, (int64_t) size, true, NULL);
   this->descriptor.nb_dim = 1;
   this->descriptor.dim[0] = size;
 }
 
 JNIEXPORT void JNICALL Java_io_greycat_impl_TensorImpl_nFillDouble(
   JNIEnv *env, jclass class, jlong ptr, jint offset, jdoubleArray data) {
-  gtensor_t *this = (gtensor_t *) (intptr_t) ptr;
+    gc_rt_tensor_t *this = (gc_rt_tensor_t *) (intptr_t) ptr;
   jdouble *raw = (*env)->GetDoubleArrayElements(env, data, false);
   jsize len = (*env)->GetArrayLength(env, data);
   if (offset + len > this->descriptor.size) {
@@ -43,7 +43,7 @@ JNIEXPORT void JNICALL Java_io_greycat_impl_TensorImpl_nFillDouble(
 
 JNIEXPORT void JNICALL Java_io_greycat_impl_TensorImpl_nFillFloat(
   JNIEnv *env, jclass class, jlong ptr, jint offset, jfloatArray data) {
-  gtensor_t *this = (gtensor_t *) (intptr_t) ptr;
+    gc_rt_tensor_t *this = (gc_rt_tensor_t *) (intptr_t) ptr;
   jfloat *raw = (*env)->GetFloatArrayElements(env, data, false);
   jsize len = (*env)->GetArrayLength(env, data);
   if (offset + len > this->descriptor.size) {
@@ -66,7 +66,7 @@ JNIEXPORT jobject JNICALL Java_io_greycat_impl_TensorImpl_nGet
         offset *= raw[i];
     }
     (*env)->ReleaseIntArrayElements(env, offsets, raw, 0);
-    gtensor_t *this = (gtensor_t *) (intptr_t) ptr;
-    gc_rt_slot_t slot = gtensor__get_value(this, offset);
+    gc_rt_tensor_t *this = (gc_rt_tensor_t *) (intptr_t) ptr;
+    gc_rt_slot_t slot = gc_rt_tensor__get_value(this, offset);
     return jtype__g2j(env, (ggraph_t *) this->header.type->graph, slot, this->descriptor.data_type);
 }
