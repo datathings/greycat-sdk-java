7e10b59cd78ca6b04b80025e5abe06d24536f294
Alexandre Garnier <alexandre.garnier@datathings.com>
2024-09-24 10:41:31 +0200

    enum & type attributes

--- src/main/java/ai/greycat/Generator.java
+++ src/main/java/ai/greycat/Generator.java
@@ -6,26 +6,75 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URL;
 import java.util.*;
-import java.util.stream.Collectors;
 
 @SuppressWarnings("IOStreamConstructor")
 public class Generator {
 
     private static final class Type {
         final int offset;
+        final String lib;
+        final String module;
         final String name;
-        final boolean isNative;
         final int genericAbiType;
+        final int g1AbiTypeDesc;
+        final int g2AbiTypeDesc;
+        final int mappedAbiTypeOffset;
+        final int maskedAbiTypeOffset;
+        final boolean isNative;
+        final boolean isEnum;
+        final GreyCat.Type.Attribute[] attributes;
 
-        private Type(int offset, String name, boolean isNative, int genericAbiType) {
+        private Type(int offset, String lib, String module, String name, int genericAbiType, int g1AbiTypeDesc, int g2AbiTypeDesc, int mappedAbiTypeOffset, int maskedAbiTypeOffset, boolean isNative, boolean isEnum, GreyCat.Type.Attribute[] attributes) {
             this.offset = offset;
+            this.lib = lib;
+            this.module = module;
             this.name = name;
-            this.isNative = isNative;
             this.genericAbiType = genericAbiType;
+            this.g1AbiTypeDesc = g1AbiTypeDesc;
+            this.g2AbiTypeDesc = g2AbiTypeDesc;
+            this.mappedAbiTypeOffset = mappedAbiTypeOffset;
+            this.maskedAbiTypeOffset = maskedAbiTypeOffset;
+            this.isNative = isNative;
+            this.isEnum = isEnum;
+            this.attributes = attributes;
         }
 
         private int getOffset() {
             return offset;
+        }
+
+        private String generics() {
+            StringBuilder builder = new StringBuilder();
+            builder.append(TYPES[g1AbiTypeDesc >>> 1].fqn());
+            if (0 != g2AbiTypeDesc) {
+                builder.append(", ").append(TYPES[g2AbiTypeDesc >>> 1].fqn());
+            }
+            return builder.toString();
+        }
+
+        private String fqn() {
+            if (CORE_NULL == offset) {
+                return "?";
+            }
+            if (CORE_ANY == offset) {
+                return "java.lang.Object";
+            }
+            if (CORE_BOOL == offset) {
+                return "java.lang.Boolean";
+            }
+            if (CORE_CHAR == offset) {
+                return "java.lang.Character";
+            }
+            if (CORE_INT == offset) {
+                return "java.lang.Long";
+            }
+            if (CORE_FLOAT == offset) {
+                return "java.lang.Double";
+            }
+            if (genericAbiType == 0) {
+                return "ai.greycat." + lib + '.' + module + '.' + name;
+            }
+            return "ai.greycat." + lib + '.' + module + '.' + name.replaceAll("<(.*)>", "<" + generics() + ">");
         }
     }
 
@@ -52,9 +101,7 @@ public class Generator {
 
     private static String[] SYMBOLS;
     private static Map<String, Library> LIBRARIES = new LinkedHashMap<>();
-    private static int[] GENERIC_ABI_TYPES;
-    private static int[] G1_ABI_TYPE_DESCS;
-    private static int[] G2_ABI_TYPE_DESCS;
+    private static Type[] TYPES;
 
     private static int CORE_NULL,
             CORE_ANY,
@@ -115,6 +162,7 @@ public class Generator {
 
         final int attributesSize = stream.read_i32();
         StringBuilder builder = new StringBuilder();
+        TYPES = new Type[typesSize];
         for (int typeOffset = 0; typeOffset < typesSize; ++typeOffset) {
             // Read
             final String moduleName = SYMBOLS[stream.read_vu32()];
@@ -142,6 +190,13 @@ public class Generator {
                     break;
             }
             final String libName = SYMBOLS[stream.read_vu32()];
+            builder.setLength(0);
+            if (moduleName != null) {
+                builder.append(moduleName);
+                builder.append("::");
+            }
+            builder.append(typeName);
+            final String fqn = builder.toString();
             final int genericAbiType = stream.read_vu32();
             final int g1AbiTypeDesc = stream.read_vu32();
             final int g2AbiTypeDesc = stream.read_vu32();
@@ -173,7 +228,10 @@ public class Generator {
             // Save
             Library lib = LIBRARIES.computeIfAbsent(libName, Library::new);
             Module module = lib.modules.computeIfAbsent(moduleName, Module::new);
-            module.types.put(typeName, new Type(typeOffset, typeName, isNative, genericAbiType));
+            Type type = new Type(typeOffset, libName, moduleName, typeName, genericAbiType, g1AbiTypeDesc,
+                    g2AbiTypeDesc, mappedAbiTypeOffset, maskedAbiTypeOffset, isNative, isEnum, typeAttributes);
+            module.types.put(typeName, type);
+            TYPES[typeOffset] = type;
         }
         // abi functions
         // TODO
@@ -203,17 +261,41 @@ public class Generator {
                 builder.append(T1).append("public static final class ").append(module.name).append(" {\n");
                 for (Type type : module.types.values()) {
                     if (0 == type.genericAbiType && !isPrimitive(type.offset)) {
-                        builder.append(T2).append("public static final class ").append(type.name);
+                        builder.append(T2).append("public static final class ").append(type.name).append(" extends ai.greycat.");
                         if (type.isNative) {
-                            builder.append(" extends ai.greycat.").append(lib.name).append("_n.").append(module.name).append('.').append(type.name);
+                            builder.append(lib.name).append("_n.").append(module.name).append('.').append(type.name);
+                        } else if (type.isEnum) {
+                            builder.append("GreyCat.Enum");
+                        } else {
+                            builder.append("GreyCat.Object");
                         }
                         builder.append(" {\n");
                         builder.append(T3).append("public static final java.lang.String name = \"").append(module.name).append("::").append(type.name).append("\";\n");
                         builder.append(T3).append("private ").append(type.name).append("(ai.greycat.GreyCat.Type type, java.lang.Object... attributes) {\n");
                         builder.append(T4).append("super(type);\n");
                         builder.append(T3).append("}\n");
+                        if (type.isEnum) {
+                            for (GreyCat.Type.Attribute attribute : type.attributes) {
+                                builder.append(T3).append("public static ai.greycat.").append(lib.name).append('.').append(module.name).append('.').append(type.name).append(' ').append(attribute.name).append("(ai.greycat.GreyCat greycat) {\n");
+                                builder.append(T4).append("final ai.greycat.GreyCat.Type t = greycat.libs_by_name.get(ai.greycat.").append(lib.name).append(".name).mapped[").append(type.offset).append("];\n");
+                                builder.append(T4).append("return (ai.greycat.").append(lib.name).append('.').append(module.name).append('.').append(type.name).append(") t.enum_values[t.generated_offsets[").append(attribute.mappedAttOffset).append("]];\n");
+                                builder.append(T3).append("}\n");
+                            }
+                        } else if (!type.isNative) {
+                            for (GreyCat.Type.Attribute attribute : type.attributes) {
+                                String fqn = TYPES[attribute.abiType].fqn();
+                                // getter
+                                builder.append(T3).append("public ").append(fqn).append(' ').append(attribute.name).append("() {\n");
+                                builder.append(T4).append("return (").append(fqn).append(") super.get(super.type.generated_offsets[").append(attribute.mappedAttOffset).append("]);\n");
+                                builder.append(T3).append("}\n");
+                                // setter
+                                builder.append(T3).append("public void set_").append(attribute.name).append('(').append(fqn).append(" v) {\n");
+                                builder.append(T4).append("super.set(super.type.generator_offsets[").append(attribute.mappedAttOffset).append("], v);\n");
+                                builder.append(T3).append("}\n");
+                            }
+                        }
                         builder.append(T3).append("public static ").append(type.name).append(" create(ai.greycat.GreyCat greycat) {\n");
-                        builder.append(T4).append("return new ").append(type.name).append("(greycat.libs_by_name.get(ai.greycat.std.name).mapped[0]);\n");
+                        builder.append(T4).append("return new ").append(type.name).append("(greycat.libs_by_name.get(ai.greycat.").append(lib.name).append(".name).mapped[").append(type.offset).append("]);\n");
                         builder.append(T3).append("}\n");
                         builder.append(T2).append("}\n");
                     }
